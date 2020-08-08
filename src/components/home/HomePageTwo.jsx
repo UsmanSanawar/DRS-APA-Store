ec7d94b222ea5207d6a8eeb6daae9934bb4e1dfa
Raven_Crafts <raufshakeel@gmail.com>
2020-08-08 18:38:08 +0500

    cccc

--- src/components/home/HomePageTwo.jsx
+++ src/components/home/HomePageTwo.jsx
@@ -1,9 +1,9 @@
 // react
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 // third-party
 import { Helmet } from 'react-helmet';
-
+import RestService from "../../store/restService/restService";
 // blocks
 import BlockBanner from '../blocks/BlockBanner';
 import BlockBrands from '../blocks/BlockBrands';
@@ -20,10 +20,24 @@ import categories from '../../data/shopBlockCategories';
 import posts from '../../data/blogPosts';
 import products from '../../data/shopProducts';
 import theme from '../../data/theme';
-import bg1 from "../../assets/imgs/door2.jpg"
+import bg1 from '../../assets/imgs/door2.jpg';
+
 
 
 function HomePageTwo() {
+
+    const [components, setComponents] = useState([]);
+
+    useEffect(()=> {
+        RestService.getWebPageComponentByPageId(6).then(res => {
+            if (res.data.status == "success") {
+                console.log(res.data.data.sort((a,b) =>  a.order-b.order ))
+
+                setComponents(res.data.data.sort((a,b) =>  a.order-b.order ))
+            }
+        })
+    },[])
+
     const columns = [
         {
             title: 'Top Rated Products',
@@ -45,99 +59,137 @@ function HomePageTwo() {
                 <title>{`Home Page Two — ${theme.name}`}</title>
             </Helmet>
 
-            {/*<div className="container">*/}
-            {/*    <h2 className='py-4' style={{ color: '#F1630C', textAlign: 'center' }}>Welcome to Automation Products & Accessories Ltd.</h2>*/}
-            {/*</div>*/}
+            {/* <div className="container"> */}
+            {/*    <h2 className='py-4' style={{ color: '#F1630C', textAlign: 'center' }}>Welcome to Automation Products & Accessories Ltd.</h2> */}
+            {/* </div> */}
 
-            {/*<BlockSlideShow />*/}
+            {/* <BlockSlideShow /> */}
 
             <div className="container mt-2">
-                <iframe width="100%" height="500px" src="https://www.youtube.com/embed/YN5pl0b13QI" frameBorder="0"
+
+                    <iframe
+                        width="100%"
+                        height="500px"
+                        src="https://www.youtube.com/embed/YN5pl0b13QI"
+                        frameBorder="0"
                         allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen />
+                        allowFullScreen
+                    />
             </div>
 
             <div className="container pb-4" style={{ marginBottom: 20 }}>
                 <div className="row pb-2">
                     <div className="col-md-3 text-justify">
-                            <p style={{ fontSize: 16, fontWeight:"bold", color:"#F1630C" }}>Specialist trade supplier of automatic door equipment for the independent companies within the commercial door market.</p>
+                        <p style={{ fontSize: 16, fontWeight: 'bold', color: '#F1630C' }}>Specialist trade supplier of automatic door equipment for the independent companies within the commercial door market.</p>
                     </div>
                     <div className="col-md-9">
                         <p>All members of APA have been in the automatic door industry for many years and have been a wide experience of many makes & models which spans the years since automatic doors where introduce to this country. APA are also suppliers of specialist automated door equipment, If you're looking high quality yet  affordable automatic door equipment then you've come to the right place.</p>
                     </div>
 
-                    {/*<div className='col-md-12 text-center mt-4 mb-3'>*/}
-                    {/*    <p style={{ fontSize: 28, fontWeight:"bold", color:"#F1630C" }}>Welcome to Automation Products & Accessories Ltd</p>*/}
-                    {/*    <p className="text-center">Specialist trade supplier of automatic door equipment for the independent companies within the commercial door market.</p>*/}
-                    {/*</div>*/}
+                    {/* <div className='col-md-12 text-center mt-4 mb-3'> */}
+                    {/*    <p style={{ fontSize: 28, fontWeight:"bold", color:"#F1630C" }}>Welcome to Automation Products & Accessories Ltd</p> */}
+                    {/*    <p className="text-center">Specialist trade supplier of automatic door equipment for the independent companies within the commercial door market.</p> */}
+                    {/* </div> */}
 
-                    <div className='col-md-12 text-center my-5'>
+                    <div className="col-md-12 text-center my-5">
                         <h2 style={{ color: '#F1630C', textAlign: 'center' }}>Widest Product Range</h2>
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing</p>
                     </div>
 
                 </div>
                 <div className="row ">
-                    <div className="col-lg-3 col-sm-6 text-center"><img
-                        src={require('../../assets/imgs/door2.jpg')}
-                        className="img-responsive"
-                        height={200}
-                        style={{ borderRadius: 20, marginBottom: 15, width: '100%', resize: 'both' }} /></div>
                     <div className="col-lg-3 col-sm-6 text-center">
-                        <img src={require('../../assets/imgs/door3.jpg')}
-                             className="img-responsive"
-                             height={200} style={{ borderRadius: 20, width: '100%', marginBottom: 15 }} /></div>
+                        <img
+                            src={require('../../assets/imgs/door2.jpg')}
+                            className="img-responsive"
+                            height={200}
+                            style={{
+                                borderRadius: 20, marginBottom: 15, width: '100%', resize: 'both',
+                            }}
+                        />
+                    </div>
+                    <div className="col-lg-3 col-sm-6 text-center">
+                        <img
+                            src={require('../../assets/imgs/door3.jpg')}
+                            className="img-responsive"
+                            height={200}
+                            style={{ borderRadius: 20, width: '100%', marginBottom: 15 }}
+                        />
+                    </div>
                     <div className="col-lg-6">
-                        <div style={{ backgroundColor: '#f1630c', borderRadius: 20, marginBottom: 15, padding: 30, height: 200, }}>
-                            <p style={{ color: '#fff', fontWeight: 'bold' }}> High quality, affordable, <br /> Automatic door</p>
+                        <div style={{
+                            backgroundColor: '#f1630c', borderRadius: 20, marginBottom: 15, padding: 30, height: 200,
+                        }}
+                        >
+                            <p style={{ color: '#fff', fontWeight: 'bold' }}>
+                                {' '}
+High quality, affordable,
+                                <br />
+                                {' '}
+Automatic door
+                            </p>
                             <h1 style={{ color: '#fff' }}>EQUIPMENTS</h1>
                         </div>
                     </div>
 
 
                     <div className="col-lg-6">
-                        <div style={{ backgroundImage: `url(${bg1})`,  borderRadius: 20, marginBottom: 15, paddingLeft: 30,paddingTop: 30, verticalAlign:'center', height: 200, }}>
+                        <div style={{
+                            backgroundImage: `url(${bg1})`, borderRadius: 20, marginBottom: 15, paddingLeft: 30, paddingTop: 30, verticalAlign: 'center', height: 200,
+                        }}
+                        >
                             <h4>Automatic</h4>
                             <h1 style={{ }}>DOOR SYSTEMS</h1>
-                            <button style={{backgroundColor: '#f1630c', borderRadius: 20, color:'#ffffff', border:'none', width: 100, padding: 5}} >Browse</button>
+                            <button style={{
+                                backgroundColor: '#f1630c', borderRadius: 20, color: '#ffffff', border: 'none', width: 100, padding: 5,
+                            }}
+                            >
+Browse
+                            </button>
                         </div>
                     </div>
 
                     <div className="col-lg-3 col-sm-6 text-center">
-                        <img src={require('../../assets/imgs/door4.jpg')}
-                             className="img-responsive" height={200}
-                             style={{ borderRadius: 20, marginBottom: 15, width: '100%', }} /></div>
+                        <img
+                            src={require('../../assets/imgs/door4.jpg')}
+                            className="img-responsive"
+                            height={200}
+                            style={{ borderRadius: 20, marginBottom: 15, width: '100%' }}
+                        />
+                    </div>
                     <div className="col-lg-3 col-sm-6 text-center">
-                        <img src={require('../../assets/imgs/doors-sliding.jpg')}
-                             height={200}
-                             style={{ borderRadius: 20, marginBottom: 15, width: '100%', }} /></div>
+                        <img
+                            src={require('../../assets/imgs/doors-sliding.jpg')}
+                            height={200}
+                            style={{ borderRadius: 20, marginBottom: 15, width: '100%' }}
+                        />
+                    </div>
                 </div>
             </div>
 
 
-
             <BlockFeatures layout="boxed" />
 
-            {/*<BlockTabbedProductsCarousel title="Featured Products" layout="grid-5" rows={2} />*/}
+            {/* <BlockTabbedProductsCarousel title="Featured Products" layout="grid-5" rows={2} /> */}
 
-            {/*<BlockBanner />*/}
+            {/* <BlockBanner /> */}
 
-            {/*<BlockProducts*/}
-            {/*    title="Bestsellers"*/}
-            {/*    layout="large-last"*/}
-            {/*    featuredProduct={products[0]}*/}
-            {/*    products={products.slice(1, 7)}*/}
-            {/*/>*/}
+            {/* <BlockProducts */}
+            {/*    title="Bestsellers" */}
+            {/*    layout="large-last" */}
+            {/*    featuredProduct={products[0]} */}
+            {/*    products={products.slice(1, 7)} */}
+            {/* /> */}
 
-            {/*<BlockCategories title="Popular Categories" layout="compact" categories={categories} />*/}
+            {/* <BlockCategories title="Popular Categories" layout="compact" categories={categories} /> */}
 
-            {/*<BlockTabbedProductsCarousel title="New Arrivals" layout="grid-5" />*/}
+            {/* <BlockTabbedProductsCarousel title="New Arrivals" layout="grid-5" /> */}
 
-            {/*<BlockPosts title="Latest News" layout="grid-nl" posts={posts} />*/}
+            {/* <BlockPosts title="Latest News" layout="grid-nl" posts={posts} /> */}
 
             <BlockBrands />
 
-            {/*<BlockProductColumns columns={columns} />*/}
+            {/* <BlockProductColumns columns={columns} /> */}
         </React.Fragment>
     );
 }
