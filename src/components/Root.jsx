ae19c3f6e7fc1bdad5e473ad41dd4c31fe8160e8
Fe4rLess <raufshakeel@gmail.com>
2020-10-06 16:06:48 +0500

    Update package-lock.json, package.json, and 3 more files...

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,37 +1,33 @@
 // react
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 // third-party
-import PropTypes from 'prop-types';
-import {
-    HashRouter,
-    Route,
-    Redirect,
-    Switch,
-} from 'react-router-dom';
-import { connect } from 'react-redux';
-import { IntlProvider } from 'react-intl';
+import PropTypes from "prop-types";
+import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
+import { connect } from "react-redux";
+import { IntlProvider } from "react-intl";
 
 // application
-import messages from '../i18n';
+import messages from "../i18n";
 
 // pages
-import Layout from './Layout';
-import HomePageOne from './home/HomePageOne';
-import HomePageTwo from './home/HomePageTwo';
-import CommonComp from "./common"
+import Layout from "./Layout";
+import HomePageOne from "./home/HomePageOne";
+import HomePageTwo from "./home/HomePageTwo";
+import CommonComp from "./common";
+import HelmetMetaData from "./common/metaComponent/MetaData";
 
 class Root extends Component {
     componentDidMount() {
         setTimeout(() => {
-            const preloader = document.querySelector('.site-preloader');
+            const preloader = document.querySelector(".site-preloader");
 
-            preloader.addEventListener('transitionend', (event) => {
-                if (event.propertyName === 'opacity') {
+            preloader.addEventListener("transitionend", (event) => {
+                if (event.propertyName === "opacity") {
                     preloader.parentNode.removeChild(preloader);
                 }
             });
-            preloader.classList.add('site-preloader__fade');
+            preloader.classList.add("site-preloader__fade");
         }, 500);
     }
 
@@ -39,25 +35,29 @@ class Root extends Component {
         const { locale } = this.props;
 
         return (
-            <IntlProvider locale={locale} messages={messages[locale]}>
-                <HashRouter basename={process.env.PUBLIC_URL}>
-                    <Switch>
-                        <Route
-                            path="/store"
-                            render={(props) => (
-                                <Layout {...props} headerLayout="compact" homeComponent={HomePageOne} />
-                            )}
-                        />
-                        <Route
-                            path="/"
-                            render={(props) => (
-                                <Layout {...props} headerLayout="default" homeComponent={HomePageTwo} />
-                            )}
-                        />
-                        <Redirect to="/" />
-                    </Switch>
-                </HashRouter>
-            </IntlProvider>
+            <div>
+                <IntlProvider locale={locale} messages={messages[locale]}>
+                    <HashRouter basename={process.env.PUBLIC_URL}>
+                        <HelmetMetaData></HelmetMetaData>
+
+                        <Switch>
+                            <Route
+                                path="/store"
+                                render={(props) => (
+                                    <Layout {...props} headerLayout="compact" homeComponent={HomePageOne} />
+                                )}
+                            />
+                            <Route
+                                path="/"
+                                render={(props) => (
+                                    <Layout {...props} headerLayout="default" homeComponent={HomePageTwo} />
+                                )}
+                            />
+                            <Redirect to="/" />
+                        </Switch>
+                    </HashRouter>
+                </IntlProvider>
+            </div>
         );
     }
 }
